9a2dd84b53519a01be7ac815620d5065d98ded5d
jv-gondim <joaovictorpgondim@gmail.com>
2022-06-13 10:29:00 -0300

    commitando exercícios

---
+++ Exercícios/Exercícios-18/ordem-crescente.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+
+    int firstNumber;
+    printf("Digite o primeiro número: ");
+    scanf("%d", &firstNumber);
+    
+    int secondNumber;
+    printf("Digite o segundo número: ");
+    scanf("%d", &secondNumber);
+
+    if(firstNumber < secondNumber){
+
+        printf("%d, %d\n", secondNumber, firstNumber);
+
+    }
+
+    if(firstNumber > secondNumber){
+
+        printf("%d, %d\n", firstNumber, secondNumber);
+
+    }
+
+    return 0;
+}
